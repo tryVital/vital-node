516174b04370557c506586310bee91f4bbda4bcd
maitham <maithamdib@gmail.com>
2021-08-10 21:38:25 +0100

    Add get workout stream

--- client/Workouts.ts
+++ client/Workouts.ts
@@ -1,5 +1,8 @@
 import { AxiosInstance } from "axios";
-import { ClientWorkoutResponse } from "./models/workout_models";
+import {
+  ClientWorkoutResponse,
+  ClientWorkoutStreamResponse,
+} from "./models/workout_models";
 
 export class WorkoutsApi {
   baseURL: string;
@@ -18,6 +21,15 @@ export class WorkoutsApi {
     const resp = await this.client.get(this.baseURL + `/workouts/${userKey}`, {
       params: { start_date: startDate, end_date: endDate, provider },
     });
+    return resp.data;
+  }
+
+  public async get_stream(
+    workoutId: string
+  ): Promise<ClientWorkoutStreamResponse> {
+    const resp = await this.client.get(
+      this.baseURL + `/workouts/${workoutId}/stream/data`
+    );
     return resp.data;
   }
 }
--- client/models/workout_models.ts
+++ client/models/workout_models.ts
@@ -1,166 +1,184 @@
 import { SourceClientFacing } from "./user_models";
 
 export interface ClientFacingSport {
-    /**
-     * 
-     * @type {number}
-     * @memberof ClientFacingSport
-     */
-    id: number;
-    /**
-     * 
-     * @type {string}
-     * @memberof ClientFacingSport
-     */
-    name: string;
+  /**
+   *
+   * @type {number}
+   * @memberof ClientFacingSport
+   */
+  id: number;
+  /**
+   *
+   * @type {string}
+   * @memberof ClientFacingSport
+   */
+  name: string;
 }
 
-
 /**
- * 
+ *
  * @export
  * @interface ClientFacingWorkout
  */
- export interface ClientFacingWorkout {
-    /**
-     * 
-     * @type {string}
-     * @memberof ClientFacingWorkout
-     */
-    id: any;
-    /**
-     * Average heart rate during workout::bpm
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    average_hr?: number;
-    /**
-     * Max heart rate during workout::bpm
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    max_hr?: number;
-    /**
-     * Cumulated distance for exercise.
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    distance?: number;
-    /**
-     * Start time of the workout::time
-     * @type {Date}
-     * @memberof ClientFacingWorkout
-     */
-    time_start: Date;
-    /**
-     * End time of the workout::time
-     * @type {Date}
-     * @memberof ClientFacingWorkout
-     */
-    time_end: Date;
-    /**
-     * Calories burned during the workout::kCal
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    calories: number;
-    /**
-     * 
-     * @type {ClientFacingSport}
-     * @memberof ClientFacingWorkout
-     */
-    sport: ClientFacingSport;
-    /**
-     * 
-     * @type {SourceClientFacing}
-     * @memberof ClientFacingWorkout
-     */
-    source: SourceClientFacing;
-    /**
-     * Time in minutes spent in different heart rate zones <50%, 50-60%, 60-70%, 70-80%, 80-90%, 90%+::seconds
-     * @type {Array&lt;number&gt;}
-     * @memberof ClientFacingWorkout
-     */
-    hr_zones: any;
-    /**
-     * User key returned by vital create user key request. This key should be stored in your database against the user and used for all interactions with the vital api.
-     * @type {string}
-     * @memberof ClientFacingWorkout
-     */
-    user_id: any;
-    /**
-     * Time spent active during the workout::seconds
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    moving_time?: any;
-    /**
-     * Elevation gain during the workout::meters
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    total_elevation_gain?: any;
-    /**
-     * Highest point of elevation::meters
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    elev_high?: any;
-    /**
-     * Lowest point of elevation::meters
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    elev_low?: any;
-    /**
-     * Average speed during workout in m/s::meters/sec
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    average_speed?: any;
-    /**
-     * Max speed during workout in m/s::meters/sec
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    max_speed?: any;
-    /**
-     * Average watts burned during exercise::watts
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    average_watts?: any;
-    /**
-     * Watts burned during exercise::watts
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    device_watts?: any;
-    /**
-     * Max watts burned during exercise::watts
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    max_watts?: any;
-    /*
-     * Weighted average watts burned during exercise::watts
-     * @type {number}
-     * @memberof ClientFacingWorkout
-     */
-    weighted_average_watts?: any;
-    /**
-     * Map of workouts encoded as polyline
-     * @type {string}
-     * @memberof ClientFacingWorkout
-     */
-    map?: any;
+export interface ClientFacingWorkout {
+  /**
+   *
+   * @type {string}
+   * @memberof ClientFacingWorkout
+   */
+  id: any;
+  /**
+   * Average heart rate during workout::bpm
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  average_hr?: number;
+  /**
+   * Max heart rate during workout::bpm
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  max_hr?: number;
+  /**
+   * Cumulated distance for exercise.
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  distance?: number;
+  /**
+   * Start time of the workout::time
+   * @type {Date}
+   * @memberof ClientFacingWorkout
+   */
+  time_start: Date;
+  /**
+   * End time of the workout::time
+   * @type {Date}
+   * @memberof ClientFacingWorkout
+   */
+  time_end: Date;
+  /**
+   * Calories burned during the workout::kCal
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  calories: number;
+  /**
+   *
+   * @type {ClientFacingSport}
+   * @memberof ClientFacingWorkout
+   */
+  sport: ClientFacingSport;
+  /**
+   *
+   * @type {SourceClientFacing}
+   * @memberof ClientFacingWorkout
+   */
+  source: SourceClientFacing;
+  /**
+   * Time in minutes spent in different heart rate zones <50%, 50-60%, 60-70%, 70-80%, 80-90%, 90%+::seconds
+   * @type {Array&lt;number&gt;}
+   * @memberof ClientFacingWorkout
+   */
+  hr_zones: any;
+  /**
+   * User key returned by vital create user key request. This key should be stored in your database against the user and used for all interactions with the vital api.
+   * @type {string}
+   * @memberof ClientFacingWorkout
+   */
+  user_id: any;
+  /**
+   * Time spent active during the workout::seconds
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  moving_time?: any;
+  /**
+   * Elevation gain during the workout::meters
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  total_elevation_gain?: any;
+  /**
+   * Highest point of elevation::meters
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  elev_high?: any;
+  /**
+   * Lowest point of elevation::meters
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  elev_low?: any;
+  /**
+   * Average speed during workout in m/s::meters/sec
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  average_speed?: any;
+  /**
+   * Max speed during workout in m/s::meters/sec
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  max_speed?: any;
+  /**
+   * Average watts burned during exercise::watts
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  average_watts?: any;
+  /**
+   * Watts burned during exercise::watts
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  device_watts?: any;
+  /**
+   * Max watts burned during exercise::watts
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  max_watts?: any;
+  /*
+   * Weighted average watts burned during exercise::watts
+   * @type {number}
+   * @memberof ClientFacingWorkout
+   */
+  weighted_average_watts?: any;
+  /**
+   * Map of workouts encoded as polyline
+   * @type {string}
+   * @memberof ClientFacingWorkout
+   */
+  map?: any;
 }
 
 export interface ClientWorkoutResponse {
-    /**
-     * 
-     * @type {Array&lt;ClientFacingWorkout&gt;}
-     * @memberof ClientWorkoutResponse
-     */
-    workouts: Array<ClientFacingWorkout>;
+  /**
+   *
+   * @type {Array&lt;ClientFacingWorkout&gt;}
+   * @memberof ClientWorkoutResponse
+   */
+  workouts: Array<ClientFacingWorkout>;
+}
+
+export interface ClientWorkoutStreamResponse {
+  /**
+   *
+   * @type {Array&lt;ClientWorkoutStreamResponse&gt;}
+   * @memberof ClientWorkoutStreamResponse
+   */
+  id: number;
+  cadence?: number[];
+  time: number[];
+  altitude?: number[];
+  velocity_smooth?: number[];
+  heartrate?: number[];
+  lat?: number[];
+  lng?: number[];
+  distance?: number[];
+  power?: number[];
+  resistance?: number[];
 }
