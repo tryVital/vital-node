b6384d7889c663670f22dafc20b81a03e3ed8224
Carlos Gabriel <Graviis@users.noreply.github.com>
2022-08-30 16:13:24 +0200

    fix: make calories optional (#27)

--- client/models/workout_models.ts
+++ client/models/workout_models.ts
@@ -62,7 +62,7 @@ export interface ClientFacingWorkout {
    * @type {number}
    * @memberof ClientFacingWorkout
    */
-  calories: number;
+  calories?: number;
   /**
    *
    * @type {ClientFacingSport}
