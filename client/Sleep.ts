8e08cb435f2caa009b40e73371a4b2596038bb70
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2023-01-25 10:34:29 +0000

    rewrite the implementation of getSleepWithStream to not use the /summary/sleep/{user_id}/stream endpoint

--- client/Sleep.ts
+++ client/Sleep.ts
@@ -1,13 +1,16 @@
 import { AxiosInstance } from 'axios';
+
 import {
   ClientSleepResponse,
   ClientSleepStreamResponse,
 } from './models/sleep_models';
 import { ClientSleepRawResponse } from './models/raw_response';
+import { utils } from './lib/utils';
 
 export class SleepApi {
   baseURL: string;
   client: AxiosInstance;
+
   constructor(baseURL: string, axios: AxiosInstance) {
     this.baseURL = baseURL;
     this.client = axios;
@@ -35,12 +38,23 @@ export class SleepApi {
     endDate?: Date,
     provider?: string
   ): Promise<ClientSleepResponse> {
+    if (utils.getDayDiff(startDate, endDate) > 7) {
+      console.warn('WARNING: calling getSleepWithStream for more than 7 days can significantly increase the latency of the request. Please consider reducing the number of days requested.');
+    }
+
     const resp = await this.client.get(
-      this.baseURL.concat(`/summary/sleep/${userId}/stream`),
+      this.baseURL.concat(`/summary/sleep/${userId}`),
       {
         params: { start_date: startDate, end_date: endDate, provider },
       }
     );
+
+    let response = resp.data as ClientSleepResponse;
+    for (let i = 0; i < response.sleep.length; i++) {
+      const stream = await this.getStream(response.sleep[i].id);
+      response.sleep[i].sleep_stream = [stream];
+    }
+
     return resp.data;
   }
 
