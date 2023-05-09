f35649756c1e594faa1ac6b99ac88ea8133886e5
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2023-05-09 15:30:07 +0100

    node client

---
+++ client/AtHomePhlebotomy.ts
@@ -0,0 +1,105 @@
+import { AxiosInstance } from 'axios';
+
+import {
+  Appointment,
+  AppointmentAvailability,
+  AppointmentLocation,
+  CancellationReason,
+  USAddress,
+} from './models/athome_phlebotomy_models';
+import { keysToCamel } from '../lib/utils';
+
+export class AtHomePhlebotomy {
+  baseURL: string;
+  client: AxiosInstance;
+
+  constructor(baseURL: string, axios: AxiosInstance) {
+    this.baseURL = baseURL;
+    this.client = axios;
+  }
+
+  public async appointmentAvailability(
+    orderId: string,
+    address: USAddress
+  ): Promise<AppointmentAvailability> {
+    const resp = await this.client.post(
+      this.baseURL.concat(
+        `/order/${orderId}/phlebotomy/appointment/availability`
+      ),
+      {
+        first_line: address.firstLine,
+        second_line: address.secondLine,
+        city: address.city,
+        state: address.state,
+        zip_code: address.zipCode,
+        unit: address.unit,
+      }
+    );
+
+    return keysToCamel(resp.data);
+  }
+
+  public async bookAppointment(
+    orderId: string,
+    bookingKey: string,
+    location: AppointmentLocation
+  ): Promise<Appointment> {
+    const resp = await this.client.post(
+      this.baseURL.concat(`/order/${orderId}/phlebotomy/appointment/book`),
+      {
+        booking_key: bookingKey,
+        location: location,
+      }
+    );
+
+    return keysToCamel(resp.data);
+  }
+
+  public async rescheduleAppointment(
+    orderId: string,
+    bookingKey: string,
+    location: AppointmentLocation
+  ): Promise<Appointment> {
+    const resp = await this.client.post(
+      this.baseURL.concat(
+        `/order/${orderId}/phlebotomy/appointment/reschedule`
+      ),
+      {
+        booking_key: bookingKey,
+        location: location,
+      }
+    );
+
+    return keysToCamel(resp.data);
+  }
+
+  public async cancelAppointment(
+    orderId: string,
+    cancellationReasonId: string
+  ): Promise<Appointment> {
+    const resp = await this.client.patch(
+      this.baseURL.concat(`/order/${orderId}/phlebotomy/appointment/cancel`),
+      {
+        cancellation_reason_id: cancellationReasonId,
+      }
+    );
+
+    return keysToCamel(resp.data);
+  }
+
+  public async cancellationReasons(): Promise<CancellationReason[]> {
+    const resp = await this.client.get(
+      this.baseURL.concat(`/order/phlebotomy/appointment/cancellation-reasons`)
+    );
+
+    return keysToCamel(resp.data);
+  }
+
+  public async getAppointment(orderId: string): Promise<Appointment> {
+    const resp = await this.client.get(
+      this.baseURL.concat(`/order/${orderId}/phlebotomy/appointment`)
+    );
+
+    return keysToCamel(resp.data);
+  }
+}
---
+++ client/models/athome_phlebotomy_models.ts
@@ -0,0 +1,58 @@
+type Type = 'phlebotomy';
+type Provider = 'getlabs';
+type Status =
+  | 'confirmed'
+  | 'pending'
+  | 'in_progress'
+  | 'completed'
+  | 'cancelled';
+
+export interface Appointment {
+  id: string;
+  userId: string;
+  address: USAddress;
+  location: AppointmentLocation;
+  startAt: string;
+  endAt: string;
+  ianaTimezone: string;
+  type: Type;
+  provider: Provider;
+  status: Status;
+  providerId: string;
+  canReschedule: boolean;
+}
+
+export interface USAddress {
+  firstLine: string;
+  secondLine?: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  unit?: string;
+}
+
+export interface AppointmentLocation {
+  lng: number;
+  lat: number;
+}
+
+export interface AppointmentAvailability {
+  timezone: string;
+  slots: AppointmentSlot[];
+}
+
+export interface AppointmentSlot {
+  bookingKey: string;
+  start: string;
+  end: string;
+  expiresAt: string;
+  price: number;
+  isPriority: boolean;
+  numAppointmentsAvailable: number;
+}
+
+export interface CancellationReason {
+  id: string;
+  name: string;
+  isRefundable: boolean;
+}
