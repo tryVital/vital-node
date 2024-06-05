62a8468660a1dae4d0d5a274d338fdf8fe9c9cbd
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-06-05 18:07:56 +0000

    Release 3.1.20

--- src/api/resources/introspect/client/Client.ts
+++ src/api/resources/introspect/client/Client.ts
@@ -61,7 +61,7 @@ export class Introspect {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.19",
+                "X-Fern-SDK-Version": "3.1.20",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -151,7 +151,7 @@ export class Introspect {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.19",
+                "X-Fern-SDK-Version": "3.1.20",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
