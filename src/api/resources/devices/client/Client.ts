ffbd53c6cf977042c3d48d5cdad84927a97e488b
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-06-06 14:09:33 +0000

    Release 3.1.23

--- src/api/resources/devices/client/Client.ts
+++ src/api/resources/devices/client/Client.ts
@@ -51,7 +51,7 @@ export class Devices {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.22",
+                "X-Fern-SDK-Version": "3.1.23",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/api/resources/introspect/client/Client.ts
+++ src/api/resources/introspect/client/Client.ts
@@ -61,7 +61,7 @@ export class Introspect {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.22",
+                "X-Fern-SDK-Version": "3.1.23",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -151,7 +151,7 @@ export class Introspect {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.22",
+                "X-Fern-SDK-Version": "3.1.23",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
