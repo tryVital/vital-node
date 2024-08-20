ee334a3af44afd9e2f820357d7e76635623cf098
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-08-20 16:39:52 +0000

    Release 3.1.60

--- src/api/resources/devices/client/Client.ts
+++ src/api/resources/devices/client/Client.ts
@@ -51,7 +51,7 @@ export class Devices {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.59",
+                "X-Fern-SDK-Version": "3.1.60",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
