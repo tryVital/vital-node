ed23af67803a9a4ac907ab594bff46e087150fde
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-08-20 10:05:16 +0000

    Release 3.1.59

--- src/api/resources/body/client/Client.ts
+++ src/api/resources/body/client/Client.ts
@@ -58,7 +58,7 @@ export class Body {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.58",
+                "X-Fern-SDK-Version": "3.1.59",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -145,7 +145,7 @@ export class Body {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.58",
+                "X-Fern-SDK-Version": "3.1.59",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/api/resources/testkit/client/Client.ts
+++ src/api/resources/testkit/client/Client.ts
@@ -40,7 +40,7 @@ export class Testkit {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.58",
+                "X-Fern-SDK-Version": "3.1.59",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -110,7 +110,7 @@ export class Testkit {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.58",
+                "X-Fern-SDK-Version": "3.1.59",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
