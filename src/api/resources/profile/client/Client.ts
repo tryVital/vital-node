e28abd5528250466b0a4e64ea3094c7bc56f876f
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-08-26 13:58:30 +0000

    Release 3.1.65

--- src/api/resources/profile/client/Client.ts
+++ src/api/resources/profile/client/Client.ts
@@ -51,7 +51,7 @@ export class Profile {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.64",
+                "X-Fern-SDK-Version": "3.1.65",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -131,7 +131,7 @@ export class Profile {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.64",
+                "X-Fern-SDK-Version": "3.1.65",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
