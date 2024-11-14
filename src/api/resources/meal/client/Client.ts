739fd284ba16ae38ab34f9e28571c06ac433adbb
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-11-14 14:16:54 +0000

    Release 3.1.110

--- src/api/resources/meal/client/Client.ts
+++ src/api/resources/meal/client/Client.ts
@@ -67,8 +67,8 @@ export class Meal {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.109",
-                "User-Agent": "@tryvital/vital-node/3.1.109",
+                "X-Fern-SDK-Version": "3.1.110",
+                "User-Agent": "@tryvital/vital-node/3.1.110",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/api/resources/profile/client/Client.ts
+++ src/api/resources/profile/client/Client.ts
@@ -60,8 +60,8 @@ export class Profile {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.109",
-                "User-Agent": "@tryvital/vital-node/3.1.109",
+                "X-Fern-SDK-Version": "3.1.110",
+                "User-Agent": "@tryvital/vital-node/3.1.110",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -148,8 +148,8 @@ export class Profile {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.109",
-                "User-Agent": "@tryvital/vital-node/3.1.109",
+                "X-Fern-SDK-Version": "3.1.110",
+                "User-Agent": "@tryvital/vital-node/3.1.110",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
