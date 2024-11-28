7c2f503c64a6ab325c51ea3f5639537d26619847
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-11-28 19:42:11 +0000

    Release 3.1.130

--- src/api/resources/aggregate/client/Client.ts
+++ src/api/resources/aggregate/client/Client.ts
@@ -68,8 +68,8 @@ export class Aggregate {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.129",
-                "User-Agent": "@tryvital/vital-node/3.1.129",
+                "X-Fern-SDK-Version": "3.1.130",
+                "User-Agent": "@tryvital/vital-node/3.1.130",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 accept: "*/*",
