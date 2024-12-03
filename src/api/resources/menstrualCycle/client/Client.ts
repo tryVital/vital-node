8bb44746d10d9eed343ed12cec47f7c695c8f503
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-12-03 16:01:33 +0000

    Release 3.1.134

--- src/api/resources/menstrualCycle/client/Client.ts
+++ src/api/resources/menstrualCycle/client/Client.ts
@@ -65,8 +65,8 @@ export class MenstrualCycle {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.133",
-                "User-Agent": "@tryvital/vital-node/3.1.133",
+                "X-Fern-SDK-Version": "3.1.134",
+                "User-Agent": "@tryvital/vital-node/3.1.134",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "3.1.133";
+export const SDK_VERSION = "3.1.134";
