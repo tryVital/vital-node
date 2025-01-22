023a1c1a2b48c5bea91c1488ac25592955a828c0
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-01-22 14:01:44 +0000

    Release 3.1.177

--- src/api/resources/menstrualCycle/client/Client.ts
+++ src/api/resources/menstrualCycle/client/Client.ts
@@ -65,8 +65,8 @@ export class MenstrualCycle {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.176",
-                "User-Agent": "@tryvital/vital-node/3.1.176",
+                "X-Fern-SDK-Version": "3.1.177",
+                "User-Agent": "@tryvital/vital-node/3.1.177",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/api/resources/testkit/client/Client.ts
+++ src/api/resources/testkit/client/Client.ts
@@ -67,8 +67,8 @@ export class Testkit {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.176",
-                "User-Agent": "@tryvital/vital-node/3.1.176",
+                "X-Fern-SDK-Version": "3.1.177",
+                "User-Agent": "@tryvital/vital-node/3.1.177",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -159,8 +159,8 @@ export class Testkit {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.176",
-                "User-Agent": "@tryvital/vital-node/3.1.176",
+                "X-Fern-SDK-Version": "3.1.177",
+                "User-Agent": "@tryvital/vital-node/3.1.177",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
