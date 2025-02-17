69385a5cd3804ff0cde9c0fa56268184f10eab4b
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-02-17 15:03:23 +0000

    Release 3.1.195

--- src/api/resources/workouts/client/Client.ts
+++ src/api/resources/workouts/client/Client.ts
@@ -67,8 +67,8 @@ export class Workouts {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.194",
-                "User-Agent": "@tryvital/vital-node/3.1.194",
+                "X-Fern-SDK-Version": "3.1.195",
+                "User-Agent": "@tryvital/vital-node/3.1.195",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -162,8 +162,8 @@ export class Workouts {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.194",
-                "User-Agent": "@tryvital/vital-node/3.1.194",
+                "X-Fern-SDK-Version": "3.1.195",
+                "User-Agent": "@tryvital/vital-node/3.1.195",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -240,8 +240,8 @@ export class Workouts {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.194",
-                "User-Agent": "@tryvital/vital-node/3.1.194",
+                "X-Fern-SDK-Version": "3.1.195",
+                "User-Agent": "@tryvital/vital-node/3.1.195",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "3.1.194";
+export const SDK_VERSION = "3.1.195";
