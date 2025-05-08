8ec924b4042dc904534360c85b738d56f5ab68d2
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-08 09:08:32 +0000

    Release 3.1.256

--- src/api/resources/electrocardiogram/client/Client.ts
+++ src/api/resources/electrocardiogram/client/Client.ts
@@ -67,8 +67,8 @@ export class Electrocardiogram {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.255",
-                "User-Agent": "@tryvital/vital-node/3.1.255",
+                "X-Fern-SDK-Version": "3.1.256",
+                "User-Agent": "@tryvital/vital-node/3.1.256",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "3.1.255";
+export const SDK_VERSION = "3.1.256";
