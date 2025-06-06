32d856dce68c8bdf7cd14308fc17be39b2967e97
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-06-06 10:56:19 +0000

    Release 3.1.284

--- src/api/resources/aggregate/client/Client.ts
+++ src/api/resources/aggregate/client/Client.ts
@@ -68,8 +68,8 @@ export class Aggregate {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.283",
-                "User-Agent": "@tryvital/vital-node/3.1.283",
+                "X-Fern-SDK-Version": "3.1.284",
+                "User-Agent": "@tryvital/vital-node/3.1.284",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 accept: "*/*",
@@ -151,8 +151,8 @@ export class Aggregate {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.283",
-                "User-Agent": "@tryvital/vital-node/3.1.283",
+                "X-Fern-SDK-Version": "3.1.284",
+                "User-Agent": "@tryvital/vital-node/3.1.284",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 accept: "*/*",
@@ -245,8 +245,8 @@ export class Aggregate {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.283",
-                "User-Agent": "@tryvital/vital-node/3.1.283",
+                "X-Fern-SDK-Version": "3.1.284",
+                "User-Agent": "@tryvital/vital-node/3.1.284",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
