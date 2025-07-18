7dac9adcdcbfdf4bd178db4013ce5c4a572f7a3b
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-18 17:55:04 +0000

    Release 3.1.311

--- src/api/resources/devices/client/Client.ts
+++ src/api/resources/devices/client/Client.ts
@@ -60,8 +60,8 @@ export class Devices {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.310",
-                "User-Agent": "@tryvital/vital-node/3.1.310",
+                "X-Fern-SDK-Version": "3.1.311",
+                "User-Agent": "@tryvital/vital-node/3.1.311",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/api/resources/sleepCycle/client/Client.ts
+++ src/api/resources/sleepCycle/client/Client.ts
@@ -67,8 +67,8 @@ export class SleepCycle {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.310",
-                "User-Agent": "@tryvital/vital-node/3.1.310",
+                "X-Fern-SDK-Version": "3.1.311",
+                "User-Agent": "@tryvital/vital-node/3.1.311",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
