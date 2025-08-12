caaec0fdb30f14472b4147c83e4aee88b79d85de
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-08-12 13:53:45 +0000

    Release 3.1.333

--- src/api/resources/insurance/client/Client.ts
+++ src/api/resources/insurance/client/Client.ts
@@ -64,8 +64,8 @@ export class Insurance {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.332",
-                "User-Agent": "@tryvital/vital-node/3.1.332",
+                "X-Fern-SDK-Version": "3.1.333",
+                "User-Agent": "@tryvital/vital-node/3.1.333",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -142,8 +142,8 @@ export class Insurance {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.332",
-                "User-Agent": "@tryvital/vital-node/3.1.332",
+                "X-Fern-SDK-Version": "3.1.333",
+                "User-Agent": "@tryvital/vital-node/3.1.333",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -225,8 +225,8 @@ export class Insurance {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.332",
-                "User-Agent": "@tryvital/vital-node/3.1.332",
+                "X-Fern-SDK-Version": "3.1.333",
+                "User-Agent": "@tryvital/vital-node/3.1.333",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
