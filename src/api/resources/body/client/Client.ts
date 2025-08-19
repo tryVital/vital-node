b5c337cb223a962d8a485fb6c3d7e97b9a21bf44
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-08-19 10:54:24 +0000

    Release 3.1.340

--- src/api/resources/body/client/Client.ts
+++ src/api/resources/body/client/Client.ts
@@ -67,8 +67,8 @@ export class Body {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.339",
-                "User-Agent": "@tryvital/vital-node/3.1.339",
+                "X-Fern-SDK-Version": "3.1.340",
+                "User-Agent": "@tryvital/vital-node/3.1.340",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -162,8 +162,8 @@ export class Body {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.339",
-                "User-Agent": "@tryvital/vital-node/3.1.339",
+                "X-Fern-SDK-Version": "3.1.340",
+                "User-Agent": "@tryvital/vital-node/3.1.340",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "3.1.339";
+export const SDK_VERSION = "3.1.340";
