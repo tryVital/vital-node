6b4f0e24bf76b726934933f9a398a92f8dc555cb
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-08-20 10:43:19 +0000

    Release 3.1.343

--- src/api/resources/activity/client/Client.ts
+++ src/api/resources/activity/client/Client.ts
@@ -67,8 +67,8 @@ export class Activity {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.342",
-                "User-Agent": "@tryvital/vital-node/3.1.342",
+                "X-Fern-SDK-Version": "3.1.343",
+                "User-Agent": "@tryvital/vital-node/3.1.343",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -162,8 +162,8 @@ export class Activity {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.342",
-                "User-Agent": "@tryvital/vital-node/3.1.342",
+                "X-Fern-SDK-Version": "3.1.343",
+                "User-Agent": "@tryvital/vital-node/3.1.343",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
