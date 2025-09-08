8b316eacda59dd11c2a398fc99bf61af6115cae4
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-08 14:35:36 +0000

    Release 3.1.364

--- src/api/resources/sleep/client/Client.ts
+++ src/api/resources/sleep/client/Client.ts
@@ -67,8 +67,8 @@ export class Sleep {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.363",
-                "User-Agent": "@tryvital/vital-node/3.1.363",
+                "X-Fern-SDK-Version": "3.1.364",
+                "User-Agent": "@tryvital/vital-node/3.1.364",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -162,8 +162,8 @@ export class Sleep {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.363",
-                "User-Agent": "@tryvital/vital-node/3.1.363",
+                "X-Fern-SDK-Version": "3.1.364",
+                "User-Agent": "@tryvital/vital-node/3.1.364",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -242,8 +242,8 @@ export class Sleep {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.363",
-                "User-Agent": "@tryvital/vital-node/3.1.363",
+                "X-Fern-SDK-Version": "3.1.364",
+                "User-Agent": "@tryvital/vital-node/3.1.364",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "3.1.363";
+export const SDK_VERSION = "3.1.364";
