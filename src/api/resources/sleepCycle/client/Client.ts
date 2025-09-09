4413f3f0268d90c47ae84ad38314020ff79ce020
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-09 08:51:23 +0000

    Release 3.1.368

--- src/api/resources/sleepCycle/client/Client.ts
+++ src/api/resources/sleepCycle/client/Client.ts
@@ -67,8 +67,8 @@ export class SleepCycle {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.367",
-                "User-Agent": "@tryvital/vital-node/3.1.367",
+                "X-Fern-SDK-Version": "3.1.368",
+                "User-Agent": "@tryvital/vital-node/3.1.368",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
