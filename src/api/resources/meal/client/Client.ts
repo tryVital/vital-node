fcb11bdeb8ac76616cf73ece3d460633146c7da7
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-10-01 12:46:14 +0000

    Release 3.1.398

--- src/api/resources/meal/client/Client.ts
+++ src/api/resources/meal/client/Client.ts
@@ -67,8 +67,8 @@ export class Meal {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@tryvital/vital-node",
-                "X-Fern-SDK-Version": "3.1.397",
-                "User-Agent": "@tryvital/vital-node/3.1.397",
+                "X-Fern-SDK-Version": "3.1.398",
+                "User-Agent": "@tryvital/vital-node/3.1.398",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "3.1.397";
+export const SDK_VERSION = "3.1.398";
